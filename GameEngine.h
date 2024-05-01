fa6b00b7ecf93ed4b687bfccec2964f3b7b14ad5
sean-mel <60356573+sean-mel@users.noreply.github.com>
2024-05-01 00:41:50 -0400

    Add files via upload

---
+++ GameEngine.h
@@ -0,0 +1,45 @@
+#ifndef GAMEENGINE_H
+#define GAMEENGINE_H
+
+#include "FieldInhabitant.h"
+#include "Veggie.h"
+#include "Captain.h"
+#include "Rabbit.h"
+
+class GameEngine {
+
+private:
+	const int NUMBEROFVEGGIES = 30;
+	const int MAXNUMBEROFRABBITS = 5;
+	const int RABBITPOINTS = 5;
+
+	FieldInahbitant*** field;
+	int height, width, score, timer;
+	Captain* captain;
+	vector<Rabbit> rabbits;
+
+	void initVeggies();
+	void initCaptain();
+	void spawnRabbits();
+	void moveCptVertical(int);
+	void moveCptHorizontal(int);
+
+public:
+	void initializeGame();
+	void intro();
+	void printField();
+	void moveRabbits();
+	void moveCaptain();
+	void timerTick();
+	void gameOver();
+	int getScore();
+	int remainingVeggies();
+
+};
+
+#endif
+
+
+
+
+
